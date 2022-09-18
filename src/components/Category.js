461e0ff6c05c50637a6b096d2ec83c78354f8d38
Bjorn Twachtmann <bjorn.twachtmann@bbc.co.uk>
2022-09-18 17:44:58 +0100

    Oversimplified basic grid pattern for icon display

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,12 +6,14 @@ import "./Category.css";
 
 const Category = () => {
   return (
-    <div>
+      <div className="category-wrapper">
       <Button className="button">CATEGORY</Button>
-      <TbBrandHtml5 />
-      <TbBrandCss3 />
-      <TbBrandJavascript />
-    </div>
+        <div className="basic-grid">
+            <div className="basic-grid-item"><TbBrandHtml5 /></div>
+            <div className="basic-grid-item"><TbBrandCss3 /></div>
+            <div className="basic-grid-item"><TbBrandJavascript /></div>
+        </div>
+      </div>
   );
 };
 
